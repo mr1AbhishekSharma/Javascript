b7bdd3881a76bb37702fb0941ebc94bda63d1762
Abhishek Sharma <131897448+mr1AbhishekSharma@users.noreply.github.com>
2024-10-10 20:14:55 +0000

    changesdone

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,18 @@
+"use strict" //use all Js code as newer version
+let age=20;
+let name="harsh"
+//alert(3+3);// it will give error as we are using node not browser
+
+//number=> 2 to power 53
+//bigint
+//string
+//boolean=>true or false
+//null=>standalone value
+//undefined
+//symbol=>unique
+
+//object
+
+console.log(typeof(name))
+console.log(typeof(undefined))//undefined
+console.log(typeof(null))//object
